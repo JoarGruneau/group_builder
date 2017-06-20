f86e796c0c702f86c2e92172e06bfaba7625e47e
Joar Gruneau <joar@gruneau.se>
2017-06-20 16:29:49 +0200

    adds permissions for creating groups

--- group_builder/apps/groups/models.py
+++ group_builder/apps/groups/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from group_builder.apps.mptt.models import MPTTModel, TreeForeignKey
-
-class Group_Base(MPTTModel):
-    creator = models.ForeignKey('auth.User')
-    name = models.CharField(max_length=50)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+from django.contrib.auth.models import User
 
 class Group(MPTTModel):
     name = models.CharField(max_length=50)
@@ -13,3 +9,22 @@ class Group(MPTTModel):
 
     class MPTTMeta:
         order_insertion_by = ['name']
+    
+    def has_permission(self, user, permission_type):
+        for perm in list(Permission.objects.filter(user = user, group__tree_id__contains=self.tree_id)):
+            print(permission_type)
+            print(perm.permission)
+            if(self.lft >= perm.group.lft and self.rght <= perm.group.rght and permission_type <= perm.permission):
+                return True
+        return False
+
+
+class Permission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,)
+
+    READ = 1
+    UPLOAD = 2
+    SUPER_USER = 3
+    permission_choice = ((READ, "read"), (UPLOAD, "upload"), (SUPER_USER, "super_user"))
+    permission = models.PositiveIntegerField(choices = permission_choice, default = READ)
--- group_builder/apps/groups/views.py
+++ group_builder/apps/groups/views.py
@@ -6,12 +6,28 @@ import group_builder.apps.groups.forms as group_forms
 
 @login_required(login_url="login/")
 def home(request):
-    return render(request,"home.html", {'nodes': group_models.Group.objects.all()})
+
+    if(request.method == "POST"):
+        parent = group_models.Group.objects.get(id = request.POST.get("parent", ""))
+
+        if(parent.has_permission(request.user, group_models.Permission.SUPER_USER)):
+            name = request.POST.get("name", "")
+            group_models.Group.objects.create(name = name, parent = parent)
+        return redirect('home')
+
+    elif(request.method == "GET"):
+        permissions = group_models.Permission.objects.filter(user = request.user)
+        groups = []
+        for permission in permissions:
+            groups = groups + list(permission.group.get_descendants(include_self=True))
+        return render(request,"home.html", {'nodes': groups})
 
 @login_required(login_url="login/")
 def create_group(request):
     if(request.method == "POST"):
-        group_models.Group.objects.create(name = request.POST.get('name', ''))
+        group = group_models.Group.objects.create(name = request.POST.get("name", ""))
+        group_models.Permission.objects.create(user = request.user, group = group, permission = group_models.Permission.SUPER_USER)
+        group_models.Group.objects.create(name = "all members", parent = group)
         return redirect('home')
 
     elif(request.method == "GET"):
